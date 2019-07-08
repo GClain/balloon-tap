52925879131232edc6757fd2f66f1a70a5233f5c
Grégory Clain <gregory.aerialgroup@gmail.com>
2019-07-08 17:04:26 +0200

    scoring to tap

--- main.lua
+++ main.lua
@@ -5,9 +5,14 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+local tapCount = 0
+
 local background = display.newImageRect("background.png", 360, 570)
 background.x = display.contentCenterX
 background.y = display.contentCenterY
+
+local tapText = display.newText(tapCount, display.contentCenterX, 20, native.systemeFont, 40)
+tapText:setFillColor(0,0,0)
 
 local platform = display.newImageRect("platform.png", 300,50)
 platform.x = display.contentCenterX
@@ -29,4 +34,8 @@ local function pushBalloon()
     -- first param = horizontal direction/ second param =vertical direction
     -- balloon.x and balloon.y, tell the physics engine where to apply the force, relative to the balloon itself
     balloon:applyLinearImpulse( 0, -0.75, balloon.x, balloon.y )
+    tapCount = tapCount +1
+    tapText.text = tapCount
 end
+
+balloon:addEventListener ("tap", pushBalloon)
